fd60325c2ae94159b15f744a17cf3f9feb35873b
Rafael Mora <rafael.mora.guti@gmail.com>
2017-10-15 22:36:55 -0500

    Added nopages updaters

--- src/react-paging.js
+++ src/react-paging.js
@@ -1,10 +1,32 @@
 import React from "react";
 
 class ReactPaging extends React.Component {
-  state = { nopages: 0, currentpage: 1, noitems: 0 };
+  state = { nopages: 0, currentpage: 1, noitems: 0, itemsperpage: 4 };
+
+  componentDidMount() {
+    const { items } = this.props;
+
+    this.setState({
+      nopages: Math.ceil(items.length / this.state.itemsperpage)
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { items } = this.props;
+
+    if (items.length != prevProps.items.length) {
+      this.setState({
+        nopages: Math.ceil(items.length / this.state.itemsperpage)
+      });
+    }
+  }
 
   render() {
-    return <div>{this.props.children(this.state)}</div>;
+    return (
+      <tr>
+        <td>{this.props.children(this.state)}</td>
+      </tr>
+    );
   }
 }
 
