413a3addeb5fa0023bdfae5e97b5810a17f56aff
Rafael Mora <rafael.mora.guti@gmail.com>
2017-10-16 14:43:28 -0500

    goBack and goFwd functions

--- src/react-paging.js
+++ src/react-paging.js
@@ -15,7 +15,8 @@ class ReactPaging extends React.Component {
     currentpage: 1,
     noitems: 0,
     itemsperpage: 4,
-    initialitem: 0
+    initialitem: 0,
+    lastitem: 4
   };
 
   componentDidMount() {
@@ -45,13 +46,63 @@ class ReactPaging extends React.Component {
     }
   }
 
+  computeBackLimits = prevpage => {
+    let newinitialitem = (prevpage - 1) * this.state.itemsperpage;
+    let newlastitem = Math.abs(prevpage * this.state.itemsperpage);
+    console.log(
+      `computeBackLimits() newinitialitem: ${newinitialitem} newlastitem: ${newlastitem}`
+    );
+    return { newinitialitem, newlastitem };
+  };
+
+  computeFwdLimits = nextpage => {
+    let newinitialitem = (nextpage - 1) * this.state.itemsperpage;
+    let newlastitem = nextpage * this.state.itemsperpage;
+    console.log(
+      `computeFwdLimits() newinitialitem: ${newinitialitem} newlastitem: ${newlastitem}`
+    );
+    return { newinitialitem, newlastitem };
+  };
+
+  goBack = () => {
+    // console.log(`goBack()`);
+    if (this.state.currentpage > 1) {
+      let prevpage = this.state.currentpage - 1;
+      let newlimits = this.computeBackLimits(prevpage);
+      // console.log(`goBack() new page: ${prevpage}`);
+      this.setState({
+        currentpage: prevpage,
+        initialitem: newlimits.newinitialitem,
+        lastitem: newlimits.newlastitem
+      });
+    }
+  };
+
+  goFwd = () => {
+    // console.log(`goFwd()`);
+    if (this.state.currentpage < this.state.nopages) {
+      let nextpage = this.state.currentpage + 1;
+      let newlimits = this.computeFwdLimits(nextpage);
+      // console.log(`goFwd() new page: ${nextpage}`);
+      this.setState({
+        currentpage: nextpage,
+        initialitem: newlimits.newinitialitem,
+        lastitem: newlimits.newlastitem
+      });
+    }
+  };
+
   render() {
     return (
       <tbody>
         {this.props.children(this.state)}
         {this.state.noitems > 0 ? (
           <tr>
-            <td colSpan="5">{`No. paginas: ${this.state.nopages}`}</td>
+            <td colSpan="5" style={{ textAlign: "center" }}>
+              <button onClick={this.goBack}>{"<"}</button>
+              {` ${this.state.currentpage}/${this.state.nopages} `}
+              <button onClick={this.goFwd}>{">"}</button>
+            </td>
           </tr>
         ) : null}
       </tbody>
