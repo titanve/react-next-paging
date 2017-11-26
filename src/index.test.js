465d4c766a33fb6303c83cf9e846a9276a3457b4
titanve <rafael.mora.guti@gmail.com>
2017-11-25 22:09:42 -0500

    chore(build): Add test:single for travis
    We don't want travis to watch the tests
    
    BREAKING CHANGE: travis

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var ReactNextPaging = require("./index");
+
+describe("react-next-paging", function() {
+  it("should work!", function() {
+    expect(true).to.be.true;
+  });
+});
