90b2d8a1640fa73813f2c99e61c90df327b75eaf
titanve <rafael.mora.guti@gmail.com>
2018-02-25 13:05:26 -0500

    test: get reactnextpaging component from index

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import ReactNextPaging, { getNoPages } from "./react-next-paging";
+import ReactNextPaging from "./index";
+import { getNoPages } from "./react-next-paging";
 
 Enzyme.configure({ adapter: new Adapter() });
 
