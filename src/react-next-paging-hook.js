89efe2edd9b78baa5a5ab93b39c7c4ae75e98350
Rafael Mora <rafael.mora.guti@gmail.com>
2019-03-03 06:32:08 -0500

    chore(export functions): export functions
    export functions

--- src/react-next-paging-hook.js
+++ src/react-next-paging-hook.js
@@ -1,52 +1,57 @@
 import React, { useState, useEffect } from "react";
 
-export function ReactNextPaging({ initialItems, initialItemsperpage, initialPagesspan }) {
+export const getNoPages = (items = [], itemsperpage) => {
+  return Math.ceil(items.length / itemsperpage);
+};
+
+export const getHalfPagesArray = pagesforarray => {
+  return Math.floor(pagesforarray / 2);
+};
+
+export const isNoPagesLargerPagesSpan = (nopages, pagesspan) => {
+  return nopages > pagesspan;
+};
+
+export const isNoEven = no => {
+  return no % 2 === 0;
+};
+
+export const getIniPageofArray = (nopages, pagesspan, page, inipagearray) => {
+  if (isNoPagesLargerPagesSpan(nopages, pagesspan)) {
+    if (page <= nopages) {
+      let halfspan = getHalfPagesArray(pagesspan);
+      if (page >= halfspan + inipagearray) {
+        let newini = page - halfspan > 0 ? page - halfspan : 1;
+        if (newini + pagesspan <= nopages) {
+          return newini;
+        } else {
+          return nopages - pagesspan + 1;
+        }
+      } else {
+        if (page > 0) {
+          return page - halfspan > 0 ? page - halfspan : 1;
+        } else {
+          return 1;
+        }
+      }
+    }
+  }
+  return inipagearray;
+};
+
+export function ReactNextPaging({
+  initialItems,
+  initialItemsperpage,
+  initialPagesspan
+}) {
   const [itemsperpage, setItemsPerPage] = useState(10);
   const [pagesspan, setPagesSpan] = useState(10);
   const [pagesforarray, setPagesForArray] = useState(10);
   const [inipagearray, setIniPageArray] = useState(1);
   const [items, setItems] = useState([]);
 
-  const getNoPages = (items = [], itemsperpage) => {
-    return Math.ceil(items.length / itemsperpage);
-  };
-
-  const getHalfPagesArray = pagesforarray => {
-    return Math.floor(pagesforarray / 2);
-  };
-
-  const isNoPagesLargerPagesSpan = (nopages, pagesspan) => {
-    return nopages > pagesspan;
-  };
-
-  const isNoEven = no => {
-    return no % 2 === 0;
-  };
-
-  const getIniPageofArray = (nopages, pagesspan, page, inipagearray) => {
-    if (isNoPagesLargerPagesSpan(nopages, pagesspan)) {
-      if (page <= nopages) {
-        let halfspan = getHalfPagesArray(pagesspan);
-        if (page >= halfspan + inipagearray) {
-          let newini = page - halfspan > 0 ? page - halfspan : 1;
-          if (newini + pagesspan <= nopages) {
-            return newini;
-          } else {
-            return nopages - pagesspan + 1;
-          }
-        } else {
-          if (page > 0) {
-            return page - halfspan > 0 ? page - halfspan : 1;
-          } else {
-            return 1;
-          }
-        }
-      }
-    }
-    return inipagearray;
-  };
-
-  generateStateFromProps = (currentpage = 1) => {
+  generateStateFromProps = (props, currentpage = 1) => {
+    const { items, itemsperpage, pagesspan } = props;
     let newnopages = getNoPages(items, itemsperpage);
     let newcurrentpage = currentpage;
     if (currentpage > newnopages) {
